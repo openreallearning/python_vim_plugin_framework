6a5a0b9f29c24444e82d061fe740d7a9a79bc184
David Kennedy S. Araújo <software@davidkennedy.dev>
2023-05-15 17:28:12 -0300

    Fix package setup to add all user modules

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='vim-python-framework',
-    version='0.1.0',
+    version='0.1.2',
     description='A framework to easily create Vim plugins using Python',
     author='David Kennedy S. Araujo',
     author_email='software@davidkennedy.dev',
-    packages=['vim_python_framework'],
+    packages=find_packages(),
     install_requires=[],
     entry_points={
         'console_scripts': [
